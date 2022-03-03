73de0bc3a79631462ff0dffda0c779eb443fd171
Pablo Buitrago <pablo@briefboards.com>
2022-03-03 09:16:55 -0300

    Updated `slogan` and `index.html` title.

--- src/components/Jumbotron.vue
+++ src/components/Jumbotron.vue
@@ -34,7 +34,8 @@ export default {
   data() {
     return {
       title: "The Trust Protocol,",
-      slogan: "Rules are not made to be broken."
+      slogan: "Rules are not to be broken.",
+      message: "Enabling the interoperability layer between multiple roots of trust."
     };
   },
 };
