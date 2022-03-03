c16e23b63b03e81abee2c1edd469a71302e4f971
Pablo Buitrago <pablo@briefboards.com>
2022-03-03 18:22:03 -0300

    Added fav icon

--- src/components/TheNavBar.vue
+++ src/components/TheNavBar.vue
@@ -3,7 +3,7 @@
     <b-navbar variant="light" type="light">
       <b-navbar-brand tag="p" class="mb-0 p-1">
         <img src="@/assets/logos/40.png" class="mb-1 pr-1" alt="verifive">
-        Verifive
+        Veri<strong>5</strong>
       </b-navbar-brand>
     </b-navbar>
   </div>
