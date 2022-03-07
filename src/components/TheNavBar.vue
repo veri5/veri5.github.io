17c2ca3580364ecfb31a72d39d3803a43214feef
Pablo Buitrago <pablo@briefboards.com>
2022-03-07 17:00:38 -0300

    Updated logo to v2.

--- src/components/TheNavBar.vue
+++ src/components/TheNavBar.vue
@@ -1,8 +1,8 @@
 <template>
   <div>
-    <b-navbar variant="light" type="light">
+    <b-navbar variant="faded" type="light">
       <b-navbar-brand tag="p" class="mb-0 p-1">
-        <img src="@/assets/logos/40.png" class="mb-1 pr-1" alt="verifive">
+        <img src="@/assets/logos/v2/original.jpeg" width="40" class="mb-1 pr-1" alt="verifive">
         Veri<strong>5</strong>
       </b-navbar-brand>
     </b-navbar>
