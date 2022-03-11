52c7d17ad0d3dccee42d7719159d69effa3a0ea1
Pablo Buitrago <pablo@briefboards.com>
2022-03-11 20:51:54 -0300

    Updated `publicPath` for `production` env.

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,8 @@
 module.exports = {
   outputDir: 'docs',
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/veri5.github.io/' 
+    : '/',
   configureWebpack: {
     module: {
       rules: [
